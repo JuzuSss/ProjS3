f856b543160d686045ccd20d5656ae5725dbe531
root <root@victor-linuxcac>
2024-10-09 22:54:52 +0200

    solveralmostdone

--- Proj/Solver/Solver.c
+++ Proj/Solver/Solver.c
@@ -1,144 +1,329 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <err.h>
-#include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-SDL_Window *create_window(char *title, unsigned int width, unsigned int height)            //ceci est le tp SDL jusqu'a 
-{                                                                                          //la ligne 71;;
-	int sdl = SDL_Init(SDL_INIT_VIDEO);
-	if(sdl != 0)
-	{
-		return NULL;
-	}
-	
-	SDL_Window *creefe = SDL_CreateWindow
-		(
-		 title,
-		 SDL_WINDOWPOS_CENTERED,
-		 SDL_WINDOWPOS_CENTERED,
-		 width,
-		 height,
-		 SDL_WINDOW_SHOWN
-		);
-	return creefe;
-};
- 
+#define MAX_LINE_LENGTH 256
 
-SDL_Renderer *create_renderer(SDL_Window *window)
+typedef struct
 {
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	return renderer;
-};
+  int x0, y0, x1, y1;
+} Position;
 
-void close_program(SDL_Window *window, SDL_Renderer *renderer)
+Position* solv_horizontal_r(char** matrice, char* mot, int i, int j, int h_len)
 {
-	SDL_Quit();
-};
-
-int process_events()
-{
-	SDL_Event event;
-	while(SDL_PollEvent(&event))
-	{
-		if(event.type == SDL_QUIT)
-		{
-			return 0;
-		}
-	}
-	return 1;
-};
-
-void draw_line(SDL_Renderer *renderer, int x1, int y1, int x2, int y2)
-{
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-};
-
-
-void draw_rects(SDL_Renderer *renderer, int rects[][4], int colors[][3], size_t len)
-{
-	for (size_t i = 0; i < len; i++)
-	{
-		SDL_SetRenderDrawColor(renderer, colors[i][0], colors[i][1], colors[i][2], 255);
-		SDL_Rect rec = 
-		{
-			rects[i][0], rects[i][1], rects[i][2], rects[i][3]
-		};
-		SDL_RenderFillRect(renderer, &rec);
-	};
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  result->x0 = i;
+  result->y0 = j;
+  result->x1 = i;
+  int w_len = strlen(mot);
+    
+  while (k < w_len && j + k < h_len && matrice[i][j + k] == mot[k])
+  {
+      k++;
+  }
+    
+  if (k == w_len)
+  {
+      result->y1 = j + k - 1;
+      return result;
+  }
+    
+  return NULL;
 }
 
-	//////////////////////////////////////////////////////////////////////////////////////////////
-
-void draw_matrice(SDL_Renderer *renderer,int rows,int cols)
+Position* solv_diag_dr(char** matrice,char* mot, int i, int j, int v_len, int h_len)
 {
-		for(int i = 1; i < cols; i++)
-		{
-			draw_line(renderer,i*100,0,i*100,cols*100);
-		}
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  result->x0 = i;
+  result->y0 = j;
+  int w_len = strlen(mot);
+  while(k < w_len && i+k < v_len && j+k < h_len && matrice[i+k][j+k] == mot[k])
+  {
+    k++;
+  }
 
-		for(int i = 1; i < rows; i++)
-		{
-			draw_line(renderer,10,i*100,rows*100,i*100);
-		}
+  if(k = w_len)
+  {
+    //TO DO
+    return result;
+  }
+  return NULL;
+}
+
+Position* solv_diag_dl(char** matrice, char* mot, int i, int j)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  result->x0 = i;
+  result->y0 = j;
+  int w_len = strlen(mot);
+    
+  while (k < w_len && i - k >= 0 && j + k < w_len && matrice[i - k][j + k] == mot[k])
+  {
+    k++;
+  }
+    
+  if (k == w_len)
+  {
+      result->x1 = i - k + 1;
+      result->y1 = j + k - 1;
+      return result;
+  }
+    
+  return NULL;
+}
+
+Position* solv_diag_ur(char** matrice, char* mot, int i, int j)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  result->x0 = i;
+  result->y0 = j;
+  int w_len = strlen(mot);
+    
+  while (k < w_len && i + k < w_len && j - k >= 0 && matrice[i + k][j - k] == mot[k])
+  {
+    k++;
+  }
+    
+  if (k == w_len)
+  {
+      result->x1 = i + k - 1;
+      result->y1 = j - k + 1;
+      return result;
+  }
+    
+  return NULL;
+}
+
+//needs to implements 4 more function
+
+Position* solv_vertical_d(char** matrice, char* mot, int i, int j, int v_len)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  //to do
+  int w_len = strlen(mot);
+  //to do    
+  return NULL;
 }
 
 
-int main(int argc, char *argv[]) {
-    // Exemple de matrice
-    char *matrice[6] = {
-        "ABCDE",
-        "FGHIJ",
-        "KLMNO",
-        "PQRST",
-        "UVWXY",
-        "UVWXY"
-    };
+Position* solv_horizontal_l(char** matrice, char* mot, int i, int j)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  //to do
+  int w_len = strlen(mot);
+  //to do    
+  return NULL;
+}
 
 
-    int rows = sizeof(matrice) / sizeof(matrice[0]);  // Nombre de lignes (5)
-    int cols = strlen(matrice[0]);                    // Nombre de colonnes (5)
+Position* solv_diag_ul(char** matrice, char* mot, int i, int j)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  //to do
+  int w_len = strlen(mot);
+  //to do    
+  return NULL;
+}
 
-    /////////////////////////////////////////////////////////////////////////////////////////
-    
-    SDL_Window *window = create_window("example", rows*100, cols*100);       //88 a 102 cree et fait apparaitre une fenetre de bonne taille
 
-    if (!window)
-        errx(EXIT_FAILURE, "Failed to create SDL window");
+Position* solv_vertical_u(char** matrice, char* mot, int i, int j)
+{
+  int k = 0;
+  Position* result = (Position*)malloc(sizeof(Position));
+  //to do
+  int w_len = strlen(mot);
+  //to do    
+  return NULL;
+}
 
-    SDL_Renderer *renderer = create_renderer(window);
 
-    if (!renderer)
-        errx(EXIT_FAILURE, "Failed to create SDL renderer");
 
-    SDL_RenderPresent(renderer); // Make the window appear
+Position* solver(char** matrice, char* mot, int v_len)
+{
+    int h_len = 0;
 
-    /////////////////////////////////////////////////////////////////////////////////////////
-
-    int rects[][4] = {                                                       //102 a 120 cree un carre gris de fond
-        { 0,0,rows*100,cols*100}};
-    
-    int colors[][3] = {
-        { 200, 200, 200 }};
-    
-    int keep_alive = 1;
-    while (keep_alive)
+    // Compter le nombre de colonnes dans la première ligne
+    while(matrice[0][h_len] != 0) 
     {
-        keep_alive = process_events();
-        
-        draw_rects(renderer, rects, colors, 3);
-        
-        // SDL_RenderPresent(renderer);
-    
-    
-    //////////////////////////////////////////////////////////////////////////////////////////
+        h_len++;
+    }
 
-				draw_matrice(renderer,rows,cols);
+    int i = 0;
+    int j = 0;
+    while(i < v_len)
+    {
+        while(j < h_len - 1)
+        {
+            if(matrice[i][j] == mot[0])
+            {
+                if(j + 1 < h_len && matrice[i][j + 1] == mot[1])
+                {
+                    Position* result = solv_horizontal_r(matrice, mot, i, j, h_len);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
 
-    		SDL_RenderPresent(renderer);
-		}		
-    close_program(window, renderer);
-    return EXIT_SUCCESS;
+                if(i + 1 < v_len && j + 1 < h_len && matrice[i + 1][j + 1] == mot[1])
+                {
+                    Position* result = solv_diag_dr(matrice, mot, i, j, v_len, h_len);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
 
-    return 0;
+                if(i + 1 < v_len && matrice[i + 1][j] == mot[1])
+                {
+                    Position* result = solv_vertical_d(matrice, mot, i, j, v_len);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+
+                if(j - 1 >= 0 && matrice[i][j - 1] == mot[1])
+                {
+                    Position* result = solv_horizontal_l(matrice, mot, i, j);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+
+                if(i - 1 >= 0 && j - 1 >= 0 && matrice[i - 1][j - 1] == mot[1])
+                {
+                    Position* result = solv_diag_ul(matrice, mot, i, j);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+
+                if(i - 1 >= 0 && matrice[i - 1][j] == mot[1])
+                {
+                    Position* result = solv_vertical_u(matrice, mot, i, j);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+
+                if(i - 1 >= 0 && j + 1 < h_len && matrice[i - 1][j + 1] == mot[1])
+                {
+                    Position* result = solv_diag_dl(matrice, mot, i, j);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+
+                if(i + 1 >= v_len && j - 1 >= 0 && matrice[i + 1][j - 1] == mot[1])
+                {
+                    Position* result = solv_diag_ur(matrice, mot, i, j);
+                    if(result != NULL)
+                    {
+                        return result;
+                    }
+                }
+            }
+            printf("matrice[%i][%i] = %c\n", j, i, matrice[i][j]);
+            j++;
+        }    
+        j = 0;
+        i++;
+    }
+    return NULL;
+}
+
+
+void toUpperCase(char *str) {
+  while(*str){
+    *str = toupper((unsigned char) *str);
+    str++;
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  if(argc != 3)
+  {
+    printf("Usage: %s <grid> <command>\n", argv[0]);
+    return 1;
+  }
+
+  FILE *file;
+  char line[MAX_LINE_LENGTH];
+  char **lines = NULL;
+  size_t count = 0;
+
+  //open files in lecture mode
+
+  file = fopen(argv[1], "r");
+  if(file == NULL){
+    fprintf(stderr, "Impossible d'ouvrire le fichier\n");
+    return 1;
+  }
+
+  //read the file line by line
+  // Lire le fichier ligne par ligne
+  while (fgets(line, sizeof(line), file)) 
+  {
+    // Allouer de la mémoire pour une nouvelle ligne
+    char *new_line = strdup(line);
+    if (new_line == NULL) 
+    {
+      fprintf(stderr, "Erreur d'allocation de mémoire\n");
+      fclose(file);
+      return 1;
+    }
+
+    // Ajouter la nouvelle ligne au tableau
+    char **temp = realloc(lines, sizeof(char *) * (count + 1));
+    if (temp == NULL) 
+    {
+      fprintf(stderr, "Erreur d'allocation de mémoire\n");
+      free(new_line);
+      fclose(file);
+      return 1;
+    }
+    lines = temp;
+    toUpperCase(new_line);
+    lines[count] = new_line;      // a check
+    count++;
+  }
+
+  fclose(file);
+
+  char *command = argv[2];
+  toUpperCase(command);
+
+  Position* result = solver(lines, command, count);
+  if(result == NULL) 
+  {
+    printf("Not found\n");
+  } 
+  else 
+  {
+    printf("(%i,%i)(%i,%i)\n", result->y0, result->x0, result->y1, result->x1);
+  }
+
+  for (size_t i = 0; i < count; i++) 
+  {
+    //printf("%s", lines[i]);
+    free(lines[i]); // Libérer la mémoire de chaque ligne
+  }
+  free(lines); // Libérer la mémoire du tableau de lignes
+
+  return result;
 }
